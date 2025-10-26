0522ff54cb0e7d2d00629bfd18d1e9fdff425493
Andrey Polulyakh <sQrun1217@mail.ru>
2025-10-26 21:20:49 +1000

    jobs list is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import JobsList from "./components/jobs-list/JobsList"
+import SummaryToday from "./components/summary-today/SummaryToday"
 
+function App() {
   return (
-    <>
-      
-    </>
+    <div className="flex justify-between">
+      <SummaryToday />
+      <JobsList />
+    </div>
   )
 }
 
--- src/components/counters-info/CountersBanner.tsx
+++ src/components/counters-info/CountersBanner.tsx
@@ -1,11 +1,11 @@
 function CountersBanner() {
   return (
-    <div className="flex justify-center group">
+    <div className="flex justify-center group mb-15 xl:mb-20">
       <div className="relative flex justify-center items-center max-w-[978px] w-full overflow-hidden min-h-21 border border-line mx-5 group-hover:border-gray-300">
         <a className="group-hover:opacity-65 duration-300" href="#">
           <img className="absolute w-50 h-[105px] left-0 top-0 translate-x-[-40%] -translate-y-[-5%]
         sm:translate-x-[-35%] md:translate-x-[-25%] lg:translate-x-[-15%] xl:translate-x-[-5%]" src="counter.svg" alt="counter" />
-          <p className="text-[21px] text-center uppercase bg-transparent z-10 w-[286px] avg:w-auto">
+          <p className="tracking-[1px] text-[21px] text-center uppercase bg-transparent z-10 w-[286px] avg:w-auto">
             Пора передавать показания счетчиков
           </p>
           <img className="absolute w-50 h-[105px] right-0 top-0 -translate-y-[30%] -translate-x-[-40%]
